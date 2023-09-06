11abff09272c45b537bb3ae967b03e1bcf813a41
Somogyi <somogyiandraska@gmail.com>
2023-09-06 13:21:56 +0200

    feltoltes

---
+++ src/kitalalosjatek/KitalalosJatek.java
@@ -0,0 +1,36 @@
+
+package kitalalosjatek;
+
+public class KitalalosJatek {
+
+    public static void main(String[] args) {
+        kirak();
+        melyik();
+        kever();
+        eredmeny();
+    }
+
+    private static void kirak() {
+        int lapok[] = new int[24];
+        int szam = 1;
+        for (int i = 0; i < lapok.length; i++) {
+            lapok[i] = szam;
+            szam++;
+            System.out.println(lapok[i]+" ");
+        }
+       
+    }
+
+    private static void melyik() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void kever() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void eredmeny() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
