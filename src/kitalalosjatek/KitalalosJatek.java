040f6840571f550199bc13672a045bebb983c4cb
Somogyvári Márton Péter <somogyvari.m.peter@dk.akkszalezi.hu>
2023-09-11 15:15:32 +0200

    harmadik oszlop eszlelese

--- src/kitalalosjatek/KitalalosJatek.java
+++ src/kitalalosjatek/KitalalosJatek.java
@@ -1,17 +1,18 @@
 package kitalalosjatek;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class KitalalosJatek {
 
     public static void main(String[] args) {
-        kirak();
-        melyik();
-        kever();
+        String lapok[] = kirak();
+        int szam = melyik();
+        kever(szam, lapok);
         eredmeny();
     }
 
-    private static void kirak() {
+    private static String[] kirak() {
         String lapok[] = new String[21];
         String szinek[] = {"P", "T", "Z", "M"};
         String[] ertekek = {"Asz", "Kir", "Fel", "X", "IX", "XIII"};
@@ -28,9 +29,11 @@ public class KitalalosJatek {
             }
             System.out.printf("%-8s", lapok[i]);
         }
+
+        return lapok;
     }
 
-    private static void melyik() {
+    private static int melyik() {
         System.out.println("");
         Scanner sc = new Scanner(System.in);
         System.out.print("Melyik oszlop (1-3): ");
@@ -39,10 +42,30 @@ public class KitalalosJatek {
             System.out.print("Melyik oszlop (1-3): ");
             szam = sc.nextInt();
         }
+
+        return szam;
     }
 
-    private static void kever() {
+    private static void kever(int szam, String[] lapok) {
+        
+        switch (szam) {
+            case 3:
+            harmadikOszlop(lapok);
+        }
+    }
 
+    private static void harmadikOszlop(String[] lapok) {
+        int sorTores = 1;
+        for (int i = 1; i < 7; i++) {
+            int index = 20 - (i - 1) * 3;
+            index = index - 3;
+            System.out.printf("%-8s", lapok[index]);
+            if (sorTores % 3 == 0) {
+                System.out.println("");
+            }
+            sorTores++;
+            
+        }
     }
 
     private static void eredmeny() {
