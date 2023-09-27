b9c5ea3124161fcda2e02bff38c0d8b45bdf4da0
Somogyi András István (SZF_2022_2024) <somogyi.andras.istvan@diak.szamalk-szalezi.hu>
2023-09-27 12:27:34 +0200

    javitas

--- src/kitalalosjatek/KitalalosJatek.java
+++ src/kitalalosjatek/KitalalosJatek.java
@@ -1,117 +1,85 @@
 package kitalalosjatek;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class KitalalosJatek {
 
+    private static String[] pakli = new String[22];
+    private static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String[] lapok = kirak();
-        int szam = melyik();
-        kever(szam, lapok);
+        feltolt();
+        for (int i = 0; i < 3; i++) {
+            kirak();
+            int oszlop = melyik();
+            kever(oszlop);
+        }
+        ezVolt();
     }
 
-    private static String[] kirak() {
-        String lapok[] = new String[22];
-        String szinek[] = {"P", "T", "Z", "M"};
-        String[] ertekek = {"Asz", "Kir", "Fel", "X", "IX", "XIII"};
-        int index = 1;
+    private static void feltolt() {
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+        int i = 1;
         for (String szin : szinek) {
-            for (int e = 0; index < 22 && e < ertekek.length; e++) {
-                lapok[index++] = szin + "_" + ertekek[e];
-
+            for (int e = 0; e < ertekek.length && i < pakli.length; e++) {
+                pakli[i++] = szin + "_" + ertekek[e];
             }
         }
-        for (int i = 1; i < lapok.length; i++) {
-            System.out.printf("%-8s", lapok[i]);
+
+    }
+
+    private static void kirak() {
+        for (int i = 1; i < pakli.length; i++) {
+            System.out.printf("%-8s", pakli[i]);
             if (i % 3 == 0) {
                 System.out.println("");
             }
         }
-
-        return lapok;
     }
 
     private static int melyik() {
-        System.out.println("");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Melyik oszlop (1-3): ");
-        int szam = sc.nextInt();
-        while (szam < 1 || szam > 3) {
-            System.out.print("Melyik oszlop (1-3): ");
-            szam = sc.nextInt();
-        }
-
-        return szam;
+        boolean jo;
+        int oszlop;
+        do {
+            System.out.print("melyik oszlop (1-3): ");
+            oszlop = sc.nextInt();
+            jo = oszlop >= 1 && oszlop <= 3;
+        } while (!jo);
+        return oszlop;
     }
 
-    private static void kever(int szam, String[] lapok) {
-        String[] tomb = new String[lapok.length];
-        switch (szam) {
+    private static void kever(int oszlop) {
+        // mindig középre a választott
+        String[] ujPakli = new String[22];
+        switch (oszlop) {
             case 1:
-                int szamlalo = 0;
-                for (int i = 1; i < 8; i++) {
-                    szamlalo++;
-                    int index = 20 - (i - 1) * 3;
-                    tomb[szamlalo] = lapok[index];
-                    index = 19 - (i - 1) * 3;
-                    tomb[szamlalo + 7] = lapok[index];
-                    index = 21 - (i - 1) * 3;
-                    tomb[szamlalo + 14] = lapok[index];
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
                 }
-                eredmeny(tomb);
                 break;
             case 2:
-                szamlalo = 0;
-                for (int i = 1; i < 8; i++) {
-                    szamlalo++;
-                    int index = 19 - (i - 1) * 3;
-                    tomb[szamlalo] = lapok[index];
-                    index = 20 - (i - 1) * 3;
-                    tomb[szamlalo + 7] = lapok[index];
-                    index = 21 - (i - 1) * 3;
-                    tomb[szamlalo + 14] = lapok[index];
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
                 }
-                eredmeny(tomb);
                 break;
             case 3:
-                harmadikOszlop(lapok);
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[21 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[20 - (i - 1) * 3];
+                }
                 break;
         }
+        pakli = ujPakli;
     }
 
-    private static void harmadikOszlop(String[] lapok) {
-        String[] csereLapok = new String[lapok.length];
-        int szamol = 1;
-
-        for (int i = 1; i < 8; i++) {
-            int index = 20 - (i - 1) * 3;
-            csereLapok[szamol] = lapok[index];
-
-            index = 21 - (i - 1) * 3;
-            csereLapok[szamol + 7] = lapok[index];
-
-            index = 19 - (i - 1) * 3;
-            csereLapok[szamol + 14] = lapok[index];
-            szamol++;
-        }
-
-        for (int i = 1; i < csereLapok.length; i++) {
-            System.out.printf("%-8s", csereLapok[i]);
-            if (i % 3 == 0) {
-                System.out.println("");
-            }
-        }
-    }
-
-    private static void eredmeny(String[] tomb) {
-        System.out.println("");
-        for (int i = 1; i < tomb.length; i++) {
-            System.out.printf("%-8s", tomb[i]);
-            if (i % 3 == 0) {
-                System.out.println("");
-            }
-        }
+    private static void ezVolt() {
+        System.out.println("A választott lap: " + pakli[11]);
     }
 
 }
